ceb88ffc7f2235638c12c65f47c06b3ebbe27b57
Pratik <pratikpandey0532@gmail.com>
2024-03-22 18:54:58 +0530

    Review section done

--- src/Sections/CustomerReviews.jsx
+++ src/Sections/CustomerReviews.jsx
@@ -1,7 +1,26 @@
+import ReviewCard from "../Componenets/ReviewCard"
+import { reviews } from "../Constants"
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold ">
+        What Our 
+        <span className="text-coral-red "> Customer's </span>
+        Say?
+      </h3>
+      <p className=" inf0-text m-auto mt-4 max-w-lg text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio officiis pariatur omnis dolore ducimus maxime placeat cumque ratione deleniti possimus iure repellendus accusantium harum obcaecati error excepturi, libero laborum debitis.</p>
+      <div className=" mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14 ">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName = {review.customerName}
+            rating = {review.rating}
+            feedback = {review.feedback}/>
+        ))}
+      </div>
+    </section>
   )
 }
 
